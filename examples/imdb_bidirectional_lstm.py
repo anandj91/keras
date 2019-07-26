951a531f32248ecf2f9b28337771ede8ea5bec9f
Anand J <anandj@cs.ubc.ca>
2019-07-25 17:19:31 -0700

    plaidml keras

--- examples/imdb_bidirectional_lstm.py
+++ examples/imdb_bidirectional_lstm.py
@@ -7,6 +7,9 @@ Time per epoch on CPU (Core i7): ~150s.
 
 from __future__ import print_function
 import numpy as np
+import plaidml.keras
+plaidml.keras.install_backend()
+import plaidml.keras.backend
 
 from keras.preprocessing import sequence
 from keras.models import Sequential
@@ -21,7 +24,10 @@ maxlen = 100
 batch_size = 32
 
 print('Loading data...')
+np_load_old = np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+np.load = np_load_old
 print(len(x_train), 'train sequences')
 print(len(x_test), 'test sequences')
 
--- examples/imdb_lstm.py
+++ examples/imdb_lstm.py
@@ -16,11 +16,16 @@ from what you see with CNNs/MLPs/etc.
 '''
 from __future__ import print_function
 
+import plaidml.keras
+plaidml.keras.install_backend()
+import plaidml.keras.backend
+
 from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Embedding
 from keras.layers import LSTM
 from keras.datasets import imdb
+import numpy as np
 
 max_features = 20000
 # cut texts after this number of words (among top max_features most common words)
@@ -28,7 +33,10 @@ maxlen = 80
 batch_size = 32
 
 print('Loading data...')
+np_load_old = np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+np.load = np_load_old
 print(len(x_train), 'train sequences')
 print(len(x_test), 'test sequences')
 
@@ -40,8 +48,8 @@ print('x_test shape:', x_test.shape)
 
 print('Build model...')
 model = Sequential()
-model.add(Embedding(max_features, 128))
-model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+model.add(Embedding(max_features, 128, input_length=maxlen))
+model.add(LSTM(128, input_shape=(maxlen, max_features), dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(1, activation='sigmoid'))
 
 # try using different optimizers and different optimizer configs
